900019c6956626f2e84f8379006334c9315f7890
mentab <25363363+mentab@users.noreply.github.com>
2019-07-18 13:36:32 +0200

    add tickformat and values

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const formatMonth = d3.timeFormat("%B");
+
 const w = 1400;
 const h = 600;
 const m = 50;
@@ -25,43 +27,46 @@ const legend = svg.append('g')
 	.attr('id', 'legend');
 
 d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
-	.then((data) => {
+	.then(data => {
 
 		const dataset = data.monthlyVariance;
 
 		console.log(dataset);
 
-		const years = d3.map(dataset, (d) => d.year).keys();
-		const months = d3.map(dataset, (d) => d.month).keys();
+		const years = d3.map(dataset, d => d.year).keys();
+		const months = d3.map(dataset, d => d.month).keys();
+
+		const min = d3.min(dataset, d => d.variance);
+		const max = d3.max(dataset, d => d.variance);
+		const middle = min + max / 2;
 
 		const colorScale = d3.scaleLinear()
-			.domain(d3.extent(dataset, (d) => d.variance))
-			.range(['blue', 'red']);
+			.domain([min, middle, max])
+			.range(['blue', 'yellow', 'red']);
 
 		const xScale = d3.scaleBand()
 			.domain(years)
-			.padding(.1)
 			.range([m, w - m]);
 
 		const yScale = d3.scaleBand()
 			.domain(months)
-			.padding(.1)
 			.range([m, h - m]);
 
 		svg.selectAll('rect')
 			.data(dataset)
 			.enter()
 			.append('rect')
-			.attr('x', (d) => xScale(d.year))
-			.attr('y', (d) => yScale(d.month))
+			.attr('x', d => xScale(d.year))
+			.attr('y', d => yScale(d.month))
 			.attr('width', xScale.bandwidth())
 			.attr('height', yScale.bandwidth())
-			.attr('fill', (d) => colorScale(d.variance));
+			.attr('fill', d => colorScale(d.variance));
 
-		const xAxis = d3.axisBottom(xScale);
+		const xAxis = d3.axisBottom(xScale)
+			.tickValues(years.filter(year => year % 10 === 0));
 
 		const yAxis = d3.axisLeft(yScale)
-			.tickFormat('toto');
+			.tickFormat(month => formatMonth(new Date(0).setMonth(month - 1)));
 
 		svg.append('g')
 			.attr('transform', 'translate(0,' + (h - m) + ')')
